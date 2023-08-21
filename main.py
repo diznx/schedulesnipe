fec1ff22e264795f1c2491f9936fd9de8e014c29
diznx <85420656+diznx@users.noreply.github.com>
2023-08-21 19:15:24 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+# loads window
+options = Options()
+# options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+options.add_experimental_option("detach", True)
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get('https://www.qgenda.com/schedule360/')
+search = driver.find_element(by=By.LINK_TEXT, value="LOGIN").click()
+time.sleep(5)
+
+# search.send_keys(Keys.RETURN)
+
+# https://apps.schedule360.com/sart
+
+# time.sleep(5)
+# driver.close()
