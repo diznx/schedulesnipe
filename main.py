5e7167c5c9bdfe7997166b4d9de626d7f5480837
diznx <85420656+diznx@users.noreply.github.com>
2023-08-23 21:41:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-# loads window
 options = Options()
 options.add_experimental_option("detach", True)
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get('https://USERNAME:PASSWORD@WEBSITE')
-time.sleep(5)
+driver.get('https://USERNAME:PASSWORD@WEBSITE.COM')
+time.sleep(3)
 
+driver.get('CERTAINWEBPAGEWITHINWEBSITE')
 select_dropdown_option = driver.find_element(By.XPATH, '//*[@name="srch"]/option[4]').click()
 press_go_button = driver.find_element(By.XPATH, '//*[@name="sub"]').click()
+time.sleep(2)
+accept_button = driver.find_element(By.XPATH, "//*[contains(@href, 'WEBSITELINK')]").click()
+time.sleep(2)
+press_schedule_now_button = driver.find_element(By.XPATH, '//*[@name="NAMEOFBUTTON"]').click()
